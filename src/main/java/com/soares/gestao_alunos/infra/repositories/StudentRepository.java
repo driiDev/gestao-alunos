a700a89bfb28bde66eee7ed988832edc673935d3
Andrielle Soares <andriellesoares76@gmail.com>
2025-06-27 19:19:09 -0300

    adicionado listar alunos pelo nome

--- src/main/java/com/soares/gestao_alunos/infra/repositories/StudentRepository.java
+++ src/main/java/com/soares/gestao_alunos/infra/repositories/StudentRepository.java
@@ -3,6 +3,11 @@ package com.soares.gestao_alunos.infra.repositories;
 import com.soares.gestao_alunos.infra.entities.Students;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Students, Integer> {
+
+
+    List<Students>findByName(String name);
 
 }
