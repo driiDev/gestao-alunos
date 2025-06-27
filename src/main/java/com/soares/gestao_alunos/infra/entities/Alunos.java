1e6eb9b3a17d9620cdecc0554f820d3c51e6fbb9
Andrielle Soares <andriellesoares76@gmail.com>
2025-06-27 00:08:48 -0300

    criação de entidade endereço

--- src/main/java/com/soares/gestao_alunos/infra/entities/Alunos.java
+++ src/main/java/com/soares/gestao_alunos/infra/entities/Alunos.java
@@ -1,4 +1,36 @@
 package com.soares.gestao_alunos.infra.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "alunos")
+@Entity
 public class Alunos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Date dateBirth;
+
+    private String cpf;
+
+    private String gender;
+
+    private String email;
+
+    @Embeddable
+    private Endereco endereco();
+
 }
