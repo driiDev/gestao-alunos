aae8c0ac65818ffcd9ba1e32384862ec997cd821
Andrielle Soares <andriellesoares76@gmail.com>
2025-06-26 23:39:35 -0300

    first commit

---
+++ src/main/java/com/soares/gestao_alunos/GestaoAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.soares.gestao_alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoAlunosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soares/gestao_alunos/GestaoAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soares.gestao_alunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
