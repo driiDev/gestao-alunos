d5d769a4608f4593b4087835e5018c96ce380456
Andrielle Soares <andriellesoares76@gmail.com>
2025-06-27 23:11:29 -0300

    adicionado metodos de "post" e "get"

--- src/main/java/com/soares/gestao_alunos/controllers/StudentController.java
+++ src/main/java/com/soares/gestao_alunos/controllers/StudentController.java
@@ -1,4 +1,35 @@
 package com.soares.gestao_alunos.controllers;
 
+import com.soares.gestao_alunos.infra.entities.Student;
+import com.soares.gestao_alunos.services.StudentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/aluno")
+@RequiredArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Void> saveStudent(@Valid @RequestBody Student student){
+        studentService.saveStudent(student);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> findByFirstName(@Valid @RequestParam String firstName){
+        return ResponseEntity.ok(studentService.findByFirstName(firstName));
+    }
+
+    @GetMapping("/todos")
+    public ResponseEntity<List<Student>> findAllStudents(){
+        return ResponseEntity.ok(studentService.findAllStudents());
+    }
+
 }
