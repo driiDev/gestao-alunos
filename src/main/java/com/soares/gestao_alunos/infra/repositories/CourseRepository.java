efe59402457d169a73eed960a24d125436eefd43
Andrielle Soares <andriellesoares76@gmail.com>
2025-06-27 22:54:34 -0300

    criacao de metodo para buscar cursos por nome

--- src/main/java/com/soares/gestao_alunos/infra/repositories/CourseRepository.java
+++ src/main/java/com/soares/gestao_alunos/infra/repositories/CourseRepository.java
@@ -3,6 +3,10 @@ package com.soares.gestao_alunos.infra.repositories;
 import com.soares.gestao_alunos.infra.entities.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CourseRepository extends JpaRepository<Course, Integer> {
 
+    List<Course> findByCourseNameIgnoreCase(String courseName);
 }
