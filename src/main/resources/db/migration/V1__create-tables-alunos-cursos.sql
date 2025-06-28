5d24934009f4e3cc83c7b820b083ff0ffa3dac32
Andrielle Soares <andriellesoares76@gmail.com>
2025-06-28 16:42:47 -0300

    fix: corrige sintaxe SQL e tipo de dados na migration

--- src/main/resources/db/migration/V1__create-tables-alunos-cursos.sql
+++ src/main/resources/db/migration/V1__create-tables-alunos-cursos.sql
@@ -5,13 +5,13 @@ CREATE TABLE students(
     data_birth DATE NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
-    gender VARCHAR(30)
+    gender VARCHAR(30),
     registration_date DATE NOT NULL
-)
+);
 
 CREATE TABLE courses(
     id INT AUTO_INCREMENT PRIMARY KEY,
     course_name VARCHAR(100) NOT NULL UNIQUE,
-    description VARCHAR(300)
-    course_duration NOT NULL
-)
+    description VARCHAR(300),
+    course_duration DOUBLE NOT NULL
+);
