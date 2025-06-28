e0c1734542b0d37f719813fe2fa27f8445e453b8
Andrielle Soares <andriellesoares76@gmail.com>
2025-06-27 23:20:08 -0300

    criacao de migracao V1

---
+++ src/main/resources/db/migration/V1__create-tables-alunos-cursos.sql
@@ -0,0 +1,17 @@
+CREATE TABLE students(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(150) NOT NULL,
+    data_birth DATE NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    gender VARCHAR(30)
+    registration_date NOT NULL
+)
+
+CREATE TABLE courses(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL UNIQUE,
+    description VARCHAR(300)
+    course_duration NOT NULL
+)
