5a1c9a95beea32dfd6c5272bc2f16113b713aa8d
Andrielle Soares <andriellesoares76@gmail.com>
2025-06-27 22:17:16 -0300

    adiciona metodo para pegar data de cadastro do aluno

--- src/main/java/com/soares/gestao_alunos/infra/entities/Student.java
+++ src/main/java/com/soares/gestao_alunos/infra/entities/Student.java
@@ -1,10 +1,11 @@
 package com.soares.gestao_alunos.infra.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import java.time.LocalDate;
-import java.util.Date;
 
 @Getter
 @Setter
@@ -13,36 +14,46 @@ import java.util.Date;
 @Builder
 @Table(name = "students")
 @Entity
-public class Students {
+public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "firstName")
+    @Column(name = "first_name")
     @NotBlank(message = "O nome não pode estar vazio")
     private String firstName;
 
-    @Column(name = "lastName")
+    @Column(name = "last_name")
     @NotBlank(message = "O sobrenome não pode estar vazio")
     private String lastName;
 
-    @Column(name = "dataBirth")
-    private Date dateBirth;
+    @Column(name = "data_birth")
+    @NotNull
+    private LocalDate dateBirth;
 
-    @Column(name = "cpf")
+    @Column(name = "cpf", unique = true)
+    @NotBlank
     private String cpf;
 
     @Column(name = "gender")
     private String gender;
 
-    @Column(name = "email")
+    @Column(name = "email", unique = true)
     @NotBlank(message = "O email não pode estar vazio")
     private String email;
 
     @Column(name = "registration_date", nullable = false, updatable = false)
     private LocalDate registrationDate;
 
+    @PrePersist
+    public void prePersist(){
+        if (registrationDate == null){
+            registrationDate = LocalDate.now();
+        }
+    }
+
+    @Valid
     @Embedded
     private Address address;
 
