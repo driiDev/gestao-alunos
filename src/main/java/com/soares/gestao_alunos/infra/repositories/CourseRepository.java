b0448a1098eef97def0019c17d4ef006d1fc5b19
Andrielle Soares <andriellesoares76@gmail.com>
2025-06-27 22:18:39 -0300

    criação de repositorio dos cursos

--- src/main/java/com/soares/gestao_alunos/infra/repositories/CourseRepository.java
+++ src/main/java/com/soares/gestao_alunos/infra/repositories/CourseRepository.java
@@ -1,8 +1,8 @@
 package com.soares.gestao_alunos.infra.repositories;
 
-import com.soares.gestao_alunos.infra.entities.Courses;
+import com.soares.gestao_alunos.infra.entities.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseRepository extends JpaRepository<Courses, Integer> {
+public interface CourseRepository extends JpaRepository<Course, Integer> {
 
 }
