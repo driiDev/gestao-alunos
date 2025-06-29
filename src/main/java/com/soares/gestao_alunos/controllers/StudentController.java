8f966ef88d9b1dcc33099cc24d9135f09d83590a
Andrielle Soares <andriellesoares76@gmail.com>
2025-06-28 22:50:44 -0300

    feat: adiciona endpoint de matrícula e corrige nome da coluna completion_date

--- src/main/java/com/soares/gestao_alunos/controllers/StudentController.java
+++ src/main/java/com/soares/gestao_alunos/controllers/StudentController.java
@@ -1,6 +1,10 @@
 package com.soares.gestao_alunos.controllers;
 
+import com.soares.gestao_alunos.infra.entities.CourseStatus;
+import com.soares.gestao_alunos.infra.entities.Enrollment;
 import com.soares.gestao_alunos.infra.entities.Student;
+import com.soares.gestao_alunos.infra.repositories.CourseRepository;
+import com.soares.gestao_alunos.infra.repositories.EnrollmentRepository;
 import com.soares.gestao_alunos.infra.repositories.StudentRepository;
 import com.soares.gestao_alunos.services.StudentService;
 import jakarta.validation.Valid;
@@ -10,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -19,10 +24,31 @@ public class StudentController {
 
     private final StudentService studentService;
     private final StudentRepository studentRepository;
+    private final CourseRepository courseRepository;
+    private final EnrollmentRepository enrollmentRepository;
 
     @PostMapping
     public ResponseEntity<Void> saveStudent(@Valid @RequestBody Student student){
         studentService.saveStudent(student);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PostMapping("/{studentId}/enroll/{courseId}")
+    public ResponseEntity<Void> enrollStudent(@PathVariable Integer studentId, @PathVariable Integer courseId){
+        var student = studentRepository.findById(studentId);
+        var course = courseRepository.findById(courseId);
+
+        if(student.isEmpty() || course.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        var enrollment = new Enrollment();
+        enrollment.setStudent(student.get());
+        enrollment.setCourse(course.get());
+        enrollment.setEnrollmentDate(LocalDate.now());
+        enrollment.setStatus(CourseStatus.IN_PROGRESS);
+
+        enrollmentRepository.save(enrollment);
+
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
