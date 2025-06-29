bf4b71e518cd7d2e01fc2e9acc81919db482b41a
Andrielle Soares <andriellesoares76@gmail.com>
2025-06-28 23:34:14 -0300

    feat: adiciona metodo para listar matriculas em cursos do aluno

--- src/main/java/com/soares/gestao_alunos/infra/repositories/EnrollmentRepository.java
+++ src/main/java/com/soares/gestao_alunos/infra/repositories/EnrollmentRepository.java
@@ -3,5 +3,8 @@ package com.soares.gestao_alunos.infra.repositories;
 import com.soares.gestao_alunos.infra.entities.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer> {
+    List<Enrollment> findByStudentId(Integer studentId);
 }
